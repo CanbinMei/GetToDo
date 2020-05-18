98f847dda7c90bdf6c8338cc33eff937693bd521
Canbin Mei <canbinmei@gmail.com>
2020-05-17 17:45:05 -0700

    NSCoder

---
+++ GetToDo/Controllers/GetToDoTableViewController.swift
@@ -0,0 +1,90 @@
+//
+//  ViewController.swift
+//  GetToDo
+//
+//  Created by Canbin Mei on 5/17/20.
+//  Copyright © 2020 Canbin Mei. All rights reserved.
+//
+
+import UIKit
+
+class GetToDoTableViewController: UITableViewController{
+    
+    var itemArray = [Item]()
+    
+    let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Item.plist")
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        loadItems()
+//        print(dataFilePath)
+    }
+    
+    @IBAction func AddButtonPressed(_ sender: UIBarButtonItem) {
+        var textField = UITextField()
+        let alert = UIAlertController(title: "Add New Item", message: "", preferredStyle: .alert)
+        
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Create new item..."
+            textField = alertTextField
+        }
+        
+        let add = UIAlertAction(title: "Add", style: .default) { (action) in
+            let newItem = Item()
+            newItem.title = textField.text!
+            self.itemArray.append(newItem)
+            self.saveItem()
+        }
+        alert.addAction(add)
+        
+        let cancel = UIAlertAction(title: "Cancel", style: .cancel) { (action) in
+            print("Cancelled")
+        }
+        alert.addAction(cancel)
+        
+        present(alert, animated: true, completion: nil)
+    }
+    
+    func saveItem() {
+        let encoder = PropertyListEncoder()
+        do {
+            let data = try encoder.encode(itemArray)
+            try data.write(to: dataFilePath!)
+        } catch {
+            print("Error encoding itemArray, \(error)")
+        }
+        self.tableView.reloadData()
+    }
+    
+    func loadItems() {
+        if let data = try? Data(contentsOf: dataFilePath!) {
+            let decoder = PropertyListDecoder()
+            do {
+                itemArray = try decoder.decode([Item].self, from: data)
+            } catch {
+                print("Error decoding itemArray, \(error)")
+            }
+        }
+    }
+    
+    // MARK: - TableView Datasource Methods
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ItemCell", for: indexPath)
+        cell.textLabel?.text = itemArray[indexPath.row].title
+        cell.accessoryType = itemArray[indexPath.row].done ? .checkmark : .none
+        return cell
+    }
+    
+    // MARK: - TableView Delegate Methods
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        itemArray[indexPath.row].done = !itemArray[indexPath.row].done
+        saveItem()
+    }
+
+}
+
---
+++ GetToDo/Models/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  GetToDo
+//
+//  Created by Canbin Mei on 5/17/20.
+//  Copyright © 2020 Canbin Mei. All rights reserved.
+//
+
+import Foundation
+
+class Item: Codable {
+    var title: String = ""
+    var done: Bool = false
+}
